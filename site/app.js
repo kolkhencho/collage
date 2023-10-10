91df1f425dcb17a4d3cccadd27ad83393e111c48
Shit <127259181+kolkhencho@users.noreply.github.com>
2023-10-11 01:17:02 +0300

    Add files via upload

---
+++ site/app.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+
+setTimeout(function(){
+
+    document.body.classList.add('loaded')
+
+    Draggable.create('.gallery',{
+        bounds:'body',
+        inertia:true
+    })
+})
+
+    
+
+}
