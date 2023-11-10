f9954c025b37fa0adc177a1d6d867406d773d43c
Shit <127259181+kolkhencho@users.noreply.github.com>
2023-11-10 15:03:25 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -27,3 +27,23 @@ window.onfocus=function(){
 window.onblur=function(){
     audio.pause();
 }
+
+const body=document.querySelector("body"),
+    toggle = document.querySelector(".toggle");
+
+    let getMode = localStorage.getItem("mode");
+    if(getMode&&getMode==="dark"){
+        body.classList.add("dark");
+        toggle.classList.add("active");
+
+    }
+
+    toggle.addEventListener("click",()=>{
+        body.classList.toggle("dark");
+
+        if(!body.classList.contains("dark")){
+            return localStorage.setItem("mode","light");
+        }
+        localStorage.setItem("mode","dark");
+    })
+    toggle.addEventListener("click",()=>toggle.classList.toggle("active"));
